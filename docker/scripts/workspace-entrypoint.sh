ac0cf33b50e563982881afdc8c1f691446fed8ef
SasaKuruppuarachchi <sasa.kuruppuarachchi@gmail.com>
2024-12-16 15:17:22 +0200

    update runagi alias path in workspace-entrypoint.sh

--- docker/scripts/workspace-entrypoint.sh
+++ docker/scripts/workspace-entrypoint.sh
@@ -18,7 +18,7 @@ echo "alias sorpx4='source /workspaces/aerostack2_ws/install/setup.bash'" >> ~/.
 echo "export AEROSTACK2_WORKSPACE=/workspaces/aerostack2_ws" >> ~/.bashrc
 echo "export PX4_FOLDER=/workspaces/aerostack2_ws/src/thirdparty/PX4-Autopilot" >> ~/.bashrc
 
-echo "alias runagi='cd /workspaces/agipix_control/src/px4_ros2_offboard/tmux/ && ./start.sh -s'" >> ~/.bashrc
+echo "alias runagi='cd /workspaces/agipix_control/src/agipix_px4_autonomy/tmux/ && ./start.sh -s'" >> ~/.bashrc
 echo "alias sorcon='source /workspaces/agipix_control/install/setup.bash'" >> ~/.bashrc
 echo "alias bilcon='cd /workspaces/agipix_control && colcon build --packages-skip px4_msgs'" >> ~/.bashrc
 echo "alias sorlidar='source /workspaces/lidar_ws/install/setup.bash'" >> ~/.bashrc
@@ -34,7 +34,7 @@ rosdep update
 # python get-pip.py
 cd /workspaces/dds/Micro-XRCE-DDS-Agent/build && sudo make install && sudo ldconfig /usr/local/lib/
 cd /workspaces/lidar_ws/src/Livox-SDK2/build && sudo make install && sudo ldconfig /usr/local/lib/
-cd /workspaces/isaac_ros-dev
+cd /workspaces/agipix_control
 #rosdep fix-permissions
 #rosdep install -y -r -q --from-paths src --ignore-src
 
